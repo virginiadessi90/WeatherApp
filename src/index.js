1cc959024338dae32c310a610ec7b2269b3d04a0
virginiadessi90 <virginia.dessi90@gmail.com>
2021-06-07 08:39:23 +0100

    Edited icon for search update

--- src/index.js
+++ src/index.js
@@ -33,6 +33,11 @@ function showTemp(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
